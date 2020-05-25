d5e8adb25ea03af94eacd0a4c36bc6c35f32b979
Ryan Northey <ryan@synca.io>
2020-05-25 04:52:16 +0100

    Add docker context

---
+++ context/docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+set -e
+
+# if the first argument look like a parameter (i.e. start with '-'), run Envoy
+if [ "${1#-}" != "$1" ]; then
+    set -- envoy "$@"
+fi
+
+if [ "$1" = 'envoy' ]; then
+    # set the log level if the $loglevel variable is set
+    if [ -n "$loglevel" ]; then
+        set -- "$@" --log-level "$loglevel"
+    fi
+fi
+
+if [ -n "$UID" ]; then
+    usermod -u "$UID" envoy
+fi
+
+if [ -n "$GID" ]; then
+    groupmod -g "$GID" envoy
+fi
+
+su-exec envoy "${@}"
